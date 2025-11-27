932641556572c577d17420a51e3554d596ab41ee
sakuraitowa <yingjingxianghe@gmail.com>
2025-11-27 11:08:57 +0900

    GemManagerの作成

---
+++ GGX2025-A/Assets/Script/GemBase.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemBase : MonoBehaviour
+{
+    public GemType type;
+
+    // ‰Šú‰»ˆ—
+    public void Initialize()
+    {
+
+    }
+}
