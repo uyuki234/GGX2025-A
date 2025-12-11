101f40c2b6fbe07ad92937a67e7bdb5771fdeac0
ringo071955 <rintokundayo@gmail.com>
2025-12-11 11:21:20 +0900

    スライダー

---
+++ GGX2025-A/Assets/Script/EnergyUIManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyUIManager : MonoBehaviour
+{
+    [SerializeField] private Slider targetSlider; // ← インスペクターで指定
+    [SerializeField] private float maxEnergy = 100f;
+    [SerializeField] private float currentEnergy = 100f;
+
+    private void Start()
+    {
+        // インスペクターで指定した値を反映
+        targetSlider.minValue = 0f;
+        targetSlider.maxValue = maxEnergy;
+        targetSlider.value = currentEnergy;
+    }
+
+    public void SetEnergy(float value)
+    {
+        currentEnergy = Mathf.Clamp(value, 0f, maxEnergy);
+        targetSlider.value = currentEnergy; // ← スライダーに反映
+    }
+
+    public void ApplyEnergy(float cost)
+    {
+        SetEnergy(currentEnergy - cost);
+    }
+
+    public void RecoverEnergy(float amount)
+    {
+        SetEnergy(currentEnergy + amount);
+    }
+}
