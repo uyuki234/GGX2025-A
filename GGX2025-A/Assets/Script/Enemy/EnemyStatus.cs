3f485fa7c94dc4a9d3a1e46217577316ddfa6af6
suzune <s2310051.ono.cgp@gmail.com>
2026-01-27 08:40:00 +0900

    ダメージ計算方法の変更と色の変更

--- GGX2025-A/Assets/Script/Enemy/EnemyStatus.cs
+++ GGX2025-A/Assets/Script/Enemy/EnemyStatus.cs
@@ -16,7 +16,10 @@ public class EnemyStatus : MonoBehaviour
     [Header("ダメージ換算の倍率")]
     public int times=2;
     [Header("enemy1であればチェック入れる")]
-    public bool ignoreFallDamage;
+    public bool enemy1;
+    private Renderer cubeRend;
+    private Color defaultColor;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,6 +28,14 @@ public class EnemyStatus : MonoBehaviour
         
         hpSlider = HPUI.transform.Find("HPBar").GetComponent<Slider>();
         hpSlider.value = 1f;
+        if(enemy1){
+            cubeRend = transform.Find("Sprite").GetComponent<Renderer>();
+        }
+        else{
+            cubeRend = GetComponent<Renderer>();
+        }
+        defaultColor = cubeRend.material.color;
+
     }
 
     void Update(){
@@ -65,6 +76,20 @@ public class EnemyStatus : MonoBehaviour
         return minSpeed;
     }
 
+    public float DamageCalculation(float dm){
+        float hp=GetHP()-dm;
+        StartCoroutine(HitColor());
+        return hp;
+    }
+
+    IEnumerator HitColor()
+    {
+      cubeRend.material.color = new Color(255f/255f, 60f/255f, 60f/255f, 220f/255f);
+      //cubeRend.material.color = new Color(225f/255f, 225f/255f, 0f, 1f);
+      yield return new WaitForSeconds(0.15f);
+      cubeRend.material.color = defaultColor;
+    }
+
     /*public bool GetAttacknum(){
         return attacknum;
     }
@@ -86,13 +111,13 @@ public class EnemyStatus : MonoBehaviour
         //速さ測定記録：1.5ブロックで約10、2ブロックで約12、3ブロックで約16、4ブロックで約19、5ブロックで約22
         if(sp>speedLimit){
             float damage = (sp-speedLimit)*times;
-            SetHP(GetHP()-damage);
+            SetHP(DamageCalculation(damage));
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground")&&!ignoreFallDamage)
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground")&&!enemy1)
         {
             //落下ダメージの速度計り
             //Debug.Log("着地時の速度: " + minSpeed);
